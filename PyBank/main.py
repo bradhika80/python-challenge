fb85b266096abe033498e31e9e76cffa7fe6c6c4
bradhika80 <bradhika80@gmail.com>
2020-08-22 13:41:46 -0500

    Add dictionary for change, calculate statistical analysis and print values

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,38 +13,50 @@ with open(budget_data) as csvfile:
     csv_header = next(csv_reader)
 
     #initialize variables
-    totalMonths = 0
     totalProfitLoses = 0.0
     previousProfitLoses = 0.0
     totalValueChanges =0.0
-
+    
+    #initialize the dictionary of the change values
+    change_dictionary = dict()
 
     # ********* processing of the records in csv starts here *********
     for row in csv_reader:
 
-        #store values
-        totalMonths = totalMonths + 1
+        #calculate total profit values
         totalProfitLoses = float(row[1]) + totalProfitLoses
 
         # assign the first row value if its the first row
-        if totalMonths == 1:
+        if len(change_dictionary) < 1:
             previousProfitLoses = float(row[1])
 
         #calculate Valuechanges 
-        totalValueChanges = (float(row[1]) - previousProfitLoses) + totalValueChanges
+        totalValueChanges = (float(row[1]) - previousProfitLoses)  
+        
+        change_dictionary.update ( {row[0] : totalValueChanges} ) 
 
-        #assign new profitLoseValue
+        #assign new profitLosesValue
         previousProfitLoses = float(row[1])
 
-
-         
-
-    
-
+        
+    # ********* processing of the records in csv ends here *********
+       
     # calculation Analysis processing
         
-    averageChange = totalValueChanges /(totalMonths -1)
-    
-    print(f"Total Months: {totalMonths}")
+    averageChange = sum(change_dictionary.values()) /(len(change_dictionary) - 1)
+
+    #calculate the min and max value from the list
+    max_key = max(change_dictionary, key=change_dictionary.get)
+    min_key = min(change_dictionary , key=change_dictionary.get)
+
+
+    # print the values in the console
+    print()
+    print("Financial Analysis")
+    print("----------------------------")
+    print()
+    print(f"Total Months: {len(change_dictionary)}")
     print(f"Total: ${int(totalProfitLoses)}")
     print(f"Average  Change: ${round(averageChange, 2)}")
+    print(f"Greatest Increase in Profits: {max_key} $({change_dictionary[max_key]})")
+    print(f"Greatest Decrease in Profits: {min_key} $({change_dictionary[min_key]})")
