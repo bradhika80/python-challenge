405a92b2ce987fcc4deb1c86f854c598b82ad168
bradhika80 <bradhika80@gmail.com>
2020-08-22 15:43:24 -0500

    Processing as dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -20,7 +20,25 @@ with open(election_data) as csvfile:
     # skip the header row 
     csv_header = next(csv_reader)
 
+    #create a list of dictionary for candidates
+    candidate_list_dictionary = dict()
+
     
+    # ********* processing of the records in csv starts here *********
+    for row in csv_reader:
+        # retrieves the count value, if available, else returns 0
+
+        candidate_count = candidate_list_dictionary.get(row[2])
+        if candidate_count is None :
+            count = 1
+        else:
+            count = candidate_count + 1
+        
+        candidate_list_dictionary.update({row[2] : count})
+
+
+    print (candidate_list_dictionary)
+
 
 
 
