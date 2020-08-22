187c5e097515cbe9ef6e0eca455c17eaba6c496d
bradhika80 <bradhika80@gmail.com>
2020-08-22 17:29:27 -0500

    Buid output directory and output to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,13 +6,12 @@
 
 #import the libraries
 
-import os
-import csv
-
+import os as os
+import csv as csv
+import math as math
 
 # declare input file path
 election_data = os.path.join("Resources", "election_data.csv")
-
 
 # open csv file
 with open(election_data) as csvfile:
@@ -24,6 +23,7 @@ with open(election_data) as csvfile:
 
     #create a dictionary for candidates
     candidate_dictionary = dict()
+    totalVoteCount = 0
 
     
     # ********* processing of the records in csv starts here *********
@@ -35,12 +35,57 @@ with open(election_data) as csvfile:
             count = 1
         else:
             count = candidate_count + 1
-
+        
         #insert/update the dictionary
-        candidate_dictionary.update ({row[2] : count})
-    
+        candidate_dictionary.update ({row[2] : count})    
+        totalVoteCount = totalVoteCount + 1
     
     # ********* processing of the records in csv ends here *********
+
+    # winner key value
+    winner_key = max(candidate_dictionary, key=candidate_dictionary.get)
+
+
+     #********** build the output results in a list *********
+    output_list = []
+    output_list.append("")
+    output_list.append("Election Results")
+    output_list.append("----------------------------")
+    output_list.append(f"Total Votes: {int(totalVoteCount)}")
+    output_list.append("---------------------------------------")
+
+    # iterating the candidate values in the dictionary
+    for key in candidate_dictionary :
+        value = candidate_dictionary[key]
+        output_list.append(f"{key} : { format(round(float(value/totalVoteCount) * 100, 2), '.3f')}% ({value})")
+
+
+    output_list.append("----------------------------")
+    output_list.append(f"Winner: {winner_key} ")
+    output_list.append("----------------------------")
+  
+
+    #********** build the output results completed *********
+
+    # printing the output to console by iterating through the list 
+    for msg in output_list :
+        print (msg) 
+
+         # printing to console is completed
+
+    # printing the output to file
+
+    #build outfile path
+    out_path = os.path.join("Analysis", "output.txt")
+
+    #create a new output file
+    with open(out_path, 'w') as outputFile:
+        for msg in output_list :
+            outputFile.writelines(msg + "\n")
+
+    # printing the output to file is completed
+
+
 
 
     
